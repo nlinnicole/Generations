b35a7d6c7c80eb6585f259a69103bf412022e9af
Nicole Lin <nlin.nicole@gmail.com>
2017-11-28 16:50:02 -0500

    Changed Room model, added fade-in and teleport

---
+++ Assets/Scripts/FadeScreen.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FadeScreen : MonoBehaviour
+{
+
+    public Texture2D fadeTexture;
+
+    [Range(0.1f, 1f)]
+    public float fadespeed;
+    public int drawDepth = -1000;
+
+    private float alpha = 1f;
+    private float fadeDir = -1f;
+
+    // Use this for initialization
+    void Start()
+    {
+        fadeIn();
+    }
+
+
+    void OnGUI()
+    {
+        alpha += fadeDir * fadespeed * Time.deltaTime;
+        alpha = Mathf.Clamp01(alpha);
+
+        Color newColor = GUI.color;
+        newColor.a = alpha;
+
+        GUI.color = newColor;
+
+        GUI.depth = drawDepth;
+
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), fadeTexture);
+    }
+
+    void fadeIn() {
+        fadeDir = -1f;
+    }
+
+    void fadeOut() {
+        fadeDir = 1f;
+    }
+}
---
+++ Assets/Scripts/Trigger_Events.cs
@@ -0,0 +1,57 @@
+﻿
+using UnityEngine;
+
+using System.Collections;
+
+[RequireComponent(typeof(Collider))]
+public class Trigger_Events : MonoBehaviour
+{
+    public Material inactiveMaterial;
+    public Material gazedAtMaterial;
+
+    public GameObject player;
+
+    public Texture2D fadeTexture;
+    [Range(0.1f, 1f)]
+    public float fadespeed;
+    public int drawDepth = -1000;
+
+    private float alpha = 1f;
+    private float fadeDir = -1f;
+
+    void Start()
+    {
+        SetGazedAt(false);
+    }
+
+    public void SetGazedAt(bool gazedAt)
+    {
+        if (inactiveMaterial != null && gazedAtMaterial != null)
+        {
+            GetComponent<Renderer>().material = gazedAt ? gazedAtMaterial : inactiveMaterial;
+            return;
+        }
+        GetComponent<Renderer>().material.color = gazedAt ? Color.green : Color.red;
+    }
+
+    public void Recenter()
+    {
+#if !UNITY_EDITOR
+    GvrCardboardHelpers.Recenter();
+#else
+        GvrEditorEmulator emulator = FindObjectOfType<GvrEditorEmulator>();
+        if (emulator == null)
+        {
+            return;
+        }
+        emulator.Recenter();
+#endif  // !UNITY_EDITOR
+    }
+
+    public void movePlayerAnimation() {
+        Debug.Log("move");
+        player.transform.position = new Vector3(0, 20, 0);
+
+    }
+
+}
