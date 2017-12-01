a0eb501572669b2d3085c7e806a3981b18d0918c
Nicole Lin <nlin.nicole@gmail.com>
2017-12-01 00:08:21 -0500

    Added deer

---
+++ Assets/Deer_Controller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deer_Controller : MonoBehaviour {
+
+    public Transform startMarker;
+    public Transform endMarker;
+    public float speed = 1.0F;
+    private float startTime;
+    private float journeyLength;
+
+    void Start()
+    {
+        startTime = Time.time;
+        journeyLength = Vector3.Distance(startMarker.position, endMarker.position);
+
+    }
+    void Update()
+    {
+        if (transform.position != endMarker.position)
+        {
+            float distCovered = (Time.time - startTime) * speed;
+            float fracJourney = distCovered / journeyLength;
+            transform.position = Vector3.Lerp(startMarker.position, endMarker.position, fracJourney);
+        }
+    }
+}
