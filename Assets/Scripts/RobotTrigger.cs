6abfdac1bbc7da277864d025a1642ced207dadfb
Nicole Lin <nlin.nicole@gmail.com>
2017-11-30 23:38:43 -0500

    Robot moves smoothly between points and added scene restart

--- Assets/Scripts/RobotTrigger.cs
+++ Assets/Scripts/RobotTrigger.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 using System.Collections;
 using System.Timers;
+using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(Collider))]
 public class RobotTrigger : MonoBehaviour
@@ -20,6 +21,12 @@ public class RobotTrigger : MonoBehaviour
     private int eventIndex = -1;
     private Vector3 playerPos = new Vector3(0, 20, 0);
 
+    public Transform destination;
+    public float journeyTime = 2F;
+
+    private Vector3 start;
+    private float startTime;
+
     void Start()
     {
         //Disable animals at start
@@ -27,43 +34,74 @@ public class RobotTrigger : MonoBehaviour
         for (int i = 0; i < animalEvents.Length; ++i) {
             animalEvents[i].SetActive(false);
         }
+
+        start = transform.position;
+        startTime = Time.time;
     }
 
     private void Update()
     {
-        if (isGazedAt)
+        if (isGazedAt && (eventIndex == -1 || robotLocations[eventIndex].position == transform.position))
         {
             timer += Time.deltaTime;
             if (timer >= gazeTime)
             {
-                transform.position = new Vector3(-5, 20, -2);
+                //teleports player at beginning
                 if (player.transform.position != playerPos) {
                     player.transform.position = playerPos;
                 }
                 timer = 0;
                 ++eventIndex;
-                if (eventIndex >= 0 && eventIndex < animalEvents.Length) {
+                if(eventIndex >= robotLocations.Length)
+                {
+                    Debug.Log("!");
+                    eventIndex = -1;
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+                    return;
+                }
+                Debug.Log(eventIndex);
+
+                //Activate animal
+                if (eventIndex >= 0 && eventIndex < animalEvents.Length)
+                {
                     animalEvents[eventIndex].SetActive(true);
-                    transform.position = robotLocations[eventIndex].position;
-                    transform.rotation = robotLocations[eventIndex].rotation;
                 }
 
+                setNewDestination(robotLocations[eventIndex]);
             }
-        } else
+        }
+        else
         {
             timer = 0;
         }
+
+        //Controls events
+        if (eventIndex == 0)
+        {
+            transform.position = robotLocations[eventIndex].position;
+        }
+        else if (eventIndex > 0 && eventIndex < robotLocations.Length)
+        {
+            Vector3 center = (destination.position + start) * 0.5F;
+            center -= new Vector3(0, 0, 1);
+            Vector3 riseRelCenter = destination.position - center;
+            Vector3 setRelCenter = start - center;
+            float fracComplete = (Time.time - startTime) / journeyTime;
+            transform.position = Vector3.Slerp(setRelCenter, riseRelCenter, fracComplete);
+            transform.position += center;
+            transform.LookAt(player.transform);
+        }
+    }
+
+    public void setNewDestination(Transform newDest) {
+        start = transform.position;
+        startTime = Time.time;
+        destination = newDest;
     }
 
     public void SetGazedAt(bool gazedAt)
     {
         isGazedAt = gazedAt;
-        //if (inactiveMaterial != null && gazedAtMaterial != null)
-        //{
-        //    GetComponent<Renderer>().material = gazedAt ? gazedAtMaterial : inactiveMaterial;
-        //    return;
-        //}
-        //GetComponent<Renderer>().material.color = gazedAt ? Color.green : Color.red;
     }
 
     public void Recenter()
